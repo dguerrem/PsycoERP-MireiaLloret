c9e8d956cad51fb36c5c34a38da8689481f014f1
dguerrem <dacormo01@hotmail.com>
2025-10-12 18:51:09 +0200

    (D) feat: Middleware deletion

--- back/routes/google/google_routes.js
+++ back/routes/google/google_routes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const googleController = require('../../controllers/google/google_controller');
 const { authenticateToken } = require('../../middlewares/auth');
 
-// Protected endpoint to generate an auth URL for the logged-in user (psychologist)
-router.get('/auth-url', authenticateToken, googleController.getAuthUrl);
+// Public endpoint to generate an auth URL. NOTE: this is public by request; in
+// production consider protecting this route to avoid abuse.
+router.get('/auth-url', googleController.getAuthUrl);
 
 // Public callback endpoint where Google will redirect with the code
 router.get('/oauth2callback', googleController.oauth2callback);
